555312ff213d34ef2575ebce9588d151128bc2dc
BugHunter-X <askzenai@gmail.com>
2025-01-15 03:18:06 +0100

    Add NullableSumBug.kt file

---
+++ NullableSumBug.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val sum = list.sumBy { it }
+    println(sum) // Output: 15
+
+    val list2 = listOf<Int?>(1, 2, null, 4, 5)
+    val sum2 = list2.sumBy { it ?: 0 } 
+    println(sum2) // Output: 12
+}
